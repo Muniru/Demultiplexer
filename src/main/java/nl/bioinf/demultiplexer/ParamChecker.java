6b2890a013ca47d5a2e0e2d4d7d9c7db27f35140
Muniru <mawal-umaru@st.hanze.nl>
2024-10-04 13:29:53 +0200

    Added: SequenceBatchThread.java Fixed: ParamChecker.java

--- src/main/java/nl/bioinf/demultiplexer/ParamChecker.java
+++ src/main/java/nl/bioinf/demultiplexer/ParamChecker.java
@@ -1,4 +1,38 @@
 package nl.bioinf.demultiplexer;
+import java.io.File;
 
 public class ParamChecker {
+    private File csvFile;
+    private File fastqFile;
+    private File outputFile;
+    private int maxMismatches;
+
+    public ParamChecker(File csvFile, File fastqFile, File outputFile, int maxMismatches) {
+        this.csvFile = csvFile;
+        this.fastqFile = fastqFile;
+        this.outputFile = outputFile;
+        this.maxMismatches = maxMismatches;
+    }
+
+    public void processFiles() {
+        processCsvFile(csvFile);
+        processFastqFile(fastqFile);
+        // Call output method if needed
+    }
+
+    public void processCsvFile(File csvFile) {
+        System.out.println("Processing CSV file: " + csvFile.getAbsolutePath());
+    }
+
+    public void processFastqFile(File fastqFile) {
+        System.out.println("Processing FASTQ file: " + fastqFile.getAbsolutePath());
+    }
+
+    public void outputFile(File outputFile) {
+        // Implementation here
+    }
+
+    public void setMaxMismatches(int maxMismatches) {
+        this.maxMismatches = maxMismatches;
+    }
 }
---
+++ src/main/java/nl/bioinf/demultiplexer/SequenceBatchThread.java
@@ -0,0 +1,19 @@
+package nl.bioinf.demultiplexer;
+
+import java.io.File;
+import java.io.FileReader;
+import java.nio.file.Path;
+import java.security.PublicKey;
+import java.util.Arrays;
+
+public class SequenceBatchThread implements Runnable {
+
+    public SequenceBatchThread(int maxError) {
+
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
