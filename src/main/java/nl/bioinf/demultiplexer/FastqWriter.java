8449803963ae4fb3d63834efbbb1d7a7f137a3ed
Muniru <muniruau@gmail.com>
2024-10-11 12:38:55 +0200

    Added: FastqWriter.concat, FastqWriter.removeFilesAndDirectories, FastqWriterTest.concat, FastqWriterTest.removeFilesAndDirectories

--- src/main/java/nl/bioinf/demultiplexer/FastqWriter.java
+++ src/main/java/nl/bioinf/demultiplexer/FastqWriter.java
@@ -1,11 +1,10 @@
 package nl.bioinf.demultiplexer;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.io.*;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.EnumSet;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -44,4 +43,77 @@ public class FastqWriter {
         return dirPath.toString();
     }
 
+    public static void concat(String outputDir, String[] fileNames, String outputResultDir) {
+        Path dirPath = Paths.get(outputDir);
+        Map<String, BufferedWriter> writers = new HashMap<>();
+
+        try {
+            // Output dir
+            Files.createDirectories(Paths.get(outputResultDir));
+
+            // Alle file/filewriters initializeren voor gebruik
+            for (String fileName : fileNames) {
+                BufferedWriter writer = new BufferedWriter(new FileWriter(Paths.get(outputResultDir, fileName).toString(), true));
+                writers.put(fileName, writer);
+            }
+
+            // "Lopen" door alle directories
+            Files.walk(dirPath)
+                    .filter(Files::isDirectory)
+                    .forEach(subDir -> {
+                        for (String fileName : fileNames) {
+                            // Voor elke fileName, find, lees en afscrijven.
+                            Path fileToConcat = subDir.resolve(fileName);
+                            if (Files.exists(fileToConcat) && Files.isRegularFile(fileToConcat)) {
+                                try (BufferedReader reader = new BufferedReader(new FileReader(fileToConcat.toString()))) {
+                                    String line;
+                                    BufferedWriter writer = writers.get(fileName);
+                                    while ((line = reader.readLine()) != null) {
+                                        writer.write(line);
+                                        writer.newLine();
+                                    }
+                                } catch (IOException e) {
+                                    throw new RuntimeException("Error reading file: " + fileToConcat, e);
+                                }
+                            }
+                        }
+                    });
+
+            // Sluit de writers
+            for (BufferedWriter writer : writers.values()) {
+                writer.close();
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException("Error concatenating files", e);
+        }
+
+    }
+
+    public static void removeFilesAndDirectories(String directoryPath) throws IOException {
+        Path directory = Paths.get(directoryPath);
+
+        // Loopt langs alle bestanden en directories om ze te verwijderen
+        Files.walkFileTree(directory, EnumSet.noneOf(FileVisitOption.class), Integer.MAX_VALUE,
+                new SimpleFileVisitor<Path>() {
+                    @Override
+                    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                        // Delete file
+                        Files.delete(file);
+                        return FileVisitResult.CONTINUE;
+                    }
+
+                    @Override
+                    public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+                        return FileVisitResult.CONTINUE;
+                    }
+
+                    @Override
+                    public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+                        // Delete directory
+                        Files.delete(dir);
+                        return FileVisitResult.CONTINUE;
+                    }
+                });
+    }
 }
--- src/test/java/nl/bioinf/demultiplexer/FastqWriterTest.java
+++ src/test/java/nl/bioinf/demultiplexer/FastqWriterTest.java
@@ -1,6 +1,8 @@
 package nl.bioinf.demultiplexer;
 
 import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.List;
@@ -20,8 +22,31 @@ class FastqWriterTest {
     @Test
     void testWrite() {
         FastqWriter fastqWriter = new FastqWriter();
-        String outputDir = Paths.get("src","test","output").toAbsolutePath().toString();
+        String outputDir = Paths.get("src","test","output", "tmp").toAbsolutePath().toString();
+        fastqWriter.write(reads(),outputDir, "0");
         fastqWriter.write(reads(),outputDir, "1");
-        System.out.println(outputDir);
+        fastqWriter.write(reads(),outputDir, "3");
+        fastqWriter.write(reads(),outputDir, "4");
+    }
+
+    @Test
+    void concat() {
+        testWrite();
+        String outputDir = Paths.get("src","test","output", "tmp").toAbsolutePath().toString();
+        String finalOutputDir = Paths.get("src","test","output").toAbsolutePath().toString();
+        String[] filenames = new String[]{ "0", "1", "2", "3" };
+        FastqWriter.concat(outputDir,filenames,finalOutputDir);
+    }
+
+    @Test
+    void removeFilesAndDirectories() {
+        concat();
+        String outputDir = Paths.get("src","test","output", "tmp").toAbsolutePath().toString();
+        try {
+            FastqWriter.removeFilesAndDirectories(outputDir);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
     }
 }
