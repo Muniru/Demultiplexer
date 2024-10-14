5aaf987a1b5fb24501beffb33778252dd3b5685b
SvenSchoonen <s.schoonen@st.hanze.nl>
2024-10-14 11:29:55 +0200

    ok

--- src/main/java/nl/bioinf/demultiplexer/StartApp.java
+++ src/main/java/nl/bioinf/demultiplexer/StartApp.java
@@ -1,9 +1,8 @@
 package nl.bioinf.demultiplexer;
 
-import picocli.CommandLine;
 import picocli.CommandLine.Command;
-import picocli.CommandLine.Parameters;
 import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
 
 import java.io.File;
 
@@ -15,16 +14,16 @@ public class StartApp implements Runnable {
 //    @Parameters(index = "0", arity = "0..1", description = "The path to the HDF5 file.")
 //    private File hdf5File = "/homes/sschoonen/Java_eindopdracht/Demultiplexer/samples/default.h5"; // Default HDF5 file path
 
-    @Parameters(index = "1", arity = "0..1", description = "The path to the CSV file mapping barcodes to samples.")
+    @Parameters(index = "0", arity = "0..1", description = "The path to the CSV file mapping barcodes to samples.")
     private File csvFile = new File("/homes/sschoonen/Java_eindopdracht/Demultiplexer/samples/default.csv");
 
-    @Parameters(index = "2", arity = "0..1", description = "The output file path.")
+    @Parameters(index = "1", arity = "0..1", description = "The output file path.")
     private File outputFile = new File("/homes/sschoonen/Java_eindopdracht/Demultiplexer/samples/output.txt"); // Default output file path
 
-    @Parameters(index = "3", arity = "0..1", description = "The fastq file path.")
+    @Parameters(index = "2", arity = "0..1", description = "The fastq file path.")
     private File fastqFile = new File("/homes/sschoonen/Java_eindopdracht/Demultiplexer/samples/default.fastq");
 
-    @Parameters(index = "4", arity = "0..1", description = "The max mismatch.")
+    @Parameters(index = "3", arity = "0..1", description = "The max mismatch.")
     private int maxMismatches = 1;
 
     @Option(names = {"-t", "--threads"}, description = "Max treads used")
